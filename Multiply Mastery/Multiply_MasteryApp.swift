8dc77bed0f9eb9cd1e4fe10e6b22dfee75c44757
Nazarzbs <famina1998@gmail.com>
2024-06-22 17:09:32 +0300

    Initial Commit

---
+++ Multiply Mastery/Multiply_MasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multiply_MasteryApp.swift
+//  Multiply Mastery
+//
+//  Created by Nazar on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Multiply_MasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
